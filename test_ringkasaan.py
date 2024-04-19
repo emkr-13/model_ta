d86c3b92b0c0b7460f07c7d7ace421d0d66ddf7a
emkr-13 <krisna.rata13@gmail.com>
2024-04-19 08:47:35 +0700

    update hari ini

--- test_ringkasaan.py
+++ test_ringkasaan.py
@@ -2,6 +2,7 @@ import pandas as pd
 from transformers import pipeline
 from concurrent.futures import ThreadPoolExecutor
 import time
+from transformers import BertTokenizer, EncoderDecoderModel
 
 def load_data(file_path):
     try:
@@ -16,8 +17,33 @@ def load_data(file_path):
 
 def summarize_text(text, summarization_pipe):
     try:
-        summarized_text = summarization_pipe(text)[0]['summary_text']
-        return summarized_text
+        start_load = time.time()
+        tokenizer = BertTokenizer.from_pretrained("cahya/bert2gpt-indonesian-summarization")
+        tokenizer.bos_token = tokenizer.cls_token
+        tokenizer.eos_token = tokenizer.sep_token
+        model = EncoderDecoderModel.from_pretrained("cahya/bert2gpt-indonesian-summarization")
+
+
+        # generate summary
+        input_ids = tokenizer.encode(text, return_tensors='pt')
+        summary_ids = model.generate(input_ids,
+                        min_length=20,
+                        max_length=80, 
+                        num_beams=10,
+                        repetition_penalty=2.5, 
+                        length_penalty=1.0, 
+                        early_stopping=True,
+                        no_repeat_ngram_size=2,
+                        use_cache=True,
+                        do_sample = True,
+                        temperature = 0.8,
+                        top_k = 50,
+                        top_p = 0.95)
+
+        summarize_text = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+        end_load = time.time()
+        print("Text summirized successfully in {:.2f} seconds.".format(end_load - start_load))
+        return summarize_text
     except Exception as e:
         print("Error occurred while summarizing text:", e)
         return None
@@ -32,7 +58,8 @@ def save_data(data, file_path):
         print("Error occurred while saving the summarized data:", e)
 
 def main():
-    file_path = 'processed_data_all.csv'
+    start_load = time.time()
+    file_path = 'processed_data_baru_300.csv'
     output_file_path = 'summarized_data.csv'
 
     # Load the data
@@ -44,7 +71,7 @@ def main():
     summarization_pipe = pipeline("summarization", model="cahya/bert2gpt-indonesian-summarization")
 
     # Create a ThreadPoolExecutor for multithreading
-    with ThreadPoolExecutor() as executor:
+    with ThreadPoolExecutor(max_workers=3) as executor:
         try:
             start_process = time.time()
             # Use map to apply the summarize_text function to each text in the dataframe
@@ -60,6 +87,8 @@ def main():
 
     # Save the dataframe to a new CSV file
     save_data(data, output_file_path)
+    end_load = time.time()
+    print("Waktu yang di butuh selama  successfully in {:.2f} seconds.".format(end_load - start_load))
 
 if __name__ == "__main__":
     main()
