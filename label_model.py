427ad2b581688d79dd3bdac8523956fb73c26dba
emkr-13 <krisna.rata13@gmail.com>
2024-05-16 19:05:44 +0700

    update

--- label_model.py
+++ label_model.py
@@ -3,7 +3,7 @@ import joblib
 from concurrent.futures import ThreadPoolExecutor
 
 # Load the dataset
-data = pd.read_csv('online_news_45000_clean_all.csv')
+data = pd.read_csv('online_news_50000_clean_all.csv')
 
 # Load the model data
 loaded_model_data = joblib.load('Sentimen/model_sentimen_lr.pkl')
@@ -48,4 +48,4 @@ predicted_sentiments = pd.concat(results)
 data['sentimen'] = predicted_sentiments
 
 # Save the updated dataframe to a new CSV file
-data.to_csv('online_news_45000_baru.csv', index=False)
+data.to_csv('online_news_50000_baru.csv', index=False)
